4b69fe15a9fc02d2aecfd70e8201b79deb083379
Alejandro Pernin <ale.pernin@gmail.com>
2019-08-17 13:07:51 -0300

    First Commit - Example

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api
+
+from backend_test.todo import Todo
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Todo, "/todo/<int:id>")
+
+if __name__ == "__main__":
+      app.run()
