39d6ef85a7f7158fe962ec78b64a197c9e3a3222
Alejandro Pernin <ale.pernin@gmail.com>
2019-08-17 13:28:03 -0300

    More dummy tests

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask_restful import Api
 
 from backend_test.todo import Todo
@@ -7,6 +8,20 @@ app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Todo, "/todo/<int:id>")
+
+
+aux = {
+    "1": {"data": "Data one"},
+    "2": {"data": "Data two"},
+}
+
+@app.route('/users/<user_id>', methods=['POST', 'GET', 'DELETE'])
+def user(user_id):
+    if request.method == 'GET':
+        return aux[user_id], 200
+    if request.method == 'POST':
+        return request.data, 200
+
 
 if __name__ == "__main__":
       app.run()
