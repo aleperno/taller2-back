daa9463004e61c2a1bf5ef1eba2586bba0f753ae
aleperno <apernin@onapsis.com>
2019-08-26 11:28:23 -0300

    Agrego template de sendgrid

---
+++ sendgrid_example.py
@@ -0,0 +1,44 @@
+import os
+from sendgrid import SendGridAPIClient, Personalization, Substitution
+from sendgrid.helpers.mail import Mail
+
+TEMPLATE_ID = 'd-3ea9b70314594c52b3b359d84de0601f'
+API_KEY = os.environ.get('SENDGRID_API_KEY')
+
+message_orig = Mail(
+    from_email='foodie@foodie.com',
+    to_emails='alepernin@gmail.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+
+
+message_template = Mail(
+    from_email='foodie@foodie.com',
+    to_emails='alepernin@gmail.com',
+)
+message_template.template_id=TEMPLATE_ID
+
+
+def personalize():
+    personalization = Personalization()
+    token = Substitution('token', 'abc1234')
+    personalization.add_substitution(token)
+    return personalization
+
+
+#message_template.add_personalization(personalize())
+
+message_template.dynamic_template_data = {
+    'token': 'esteesuntokendelaputamadre',
+    'Sender_Name': 'Foodie S.R.L'
+}
+
+
+try:
+    sg = SendGridAPIClient(API_KEY)
+    response = sg.send(message_template)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
