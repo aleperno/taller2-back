a6c62d3ce03cc103893c7b318780e867ff01e704
Alejandro Pernin <ale.pernin@gmail.com>
2019-12-05 13:54:37 -0300

    Puedo setear loglevel por variable de entorno

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import os
 import logging
 from models import Base, engine
 from api.admin_resources import app, check_admin_status
+from models.pricing import PricingRules
 
 
 Base.metadata.create_all(engine)
 check_admin_status()
+
+PricingRules.check_initial_status()
+
+LOGLEVEL = os.environ.get('LOGLEVEL')
 
 
 @app.before_first_request
@@ -16,6 +22,8 @@ def setup_logging():
     app.logger.handlers.extend(gunicorn_logger.handlers)
 
     # Seteo el mismo level que gunicorn
+    if LOGLEVEL:
+        gunicorn_logger.setLevel(getattr(logging, LOGLEVEL))
     app.logger.setLevel(gunicorn_logger.level)
 
     #app.logger.setLevel(logging.INFO)
